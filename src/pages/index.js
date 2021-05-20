eecf24592c56850a94f6e839fa9338251df29b7e
sourabh singh <mshrudra@gmail.com>
2021-05-20 23:32:41 +0530

    app built

---
+++ src/pages/index.js
@@ -0,0 +1,41 @@
+import Head from "next/head";
+import Banner from "../components/Banner";
+import Header from "../components/Header";
+import ProductFeed from "../components/ProductFeed";
+
+export default function Home({ products }) {
+  return (
+    <div className="bg-gray-100 ">
+      <Head>
+        <title>Amazon</title>
+      </Head>
+
+      {/* header */}
+      <Header />
+
+      <main className="max-w-screen-2xl mx-auto">
+        {/* banner */}
+
+        <Banner />
+
+        {/* products */}
+
+        <ProductFeed products={products} />
+      </main>
+    </div>
+  );
+}
+
+export async function getServerSideProps(context) {
+  const products = await fetch("https://fakestoreapi.com/products").then(
+    (res) => res.json()
+  );
+
+  return {
+    props: {
+      products,
+    },
+  };
+}
+
+//https://fakestoreapi.com/products
