388c6b2150814ddba3c12c8e82f7754c83ea9098
sourabh singh <mshrudra@gmail.com>
2021-05-22 00:36:00 +0530

    day3

---
+++ firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCaUfn9SRZB7vIDyYuCD7YkFlqLOgBlL94",
+  authDomain: "clone-e57ea.firebaseapp.com",
+  projectId: "clone-e57ea",
+  storageBucket: "clone-e57ea.appspot.com",
+  messagingSenderId: "327595642966",
+  appId: "1:327595642966:web:84ea56fa86fbda14716bf7",
+  measurementId: "G-040YTKQMRC",
+};
---
+++ src/components/CheckoutProduct.js
@@ -0,0 +1,81 @@
+import { StarIcon } from "@heroicons/react/solid";
+import Image from "next/image";
+import React from "react";
+import Currency from "react-currency-formatter";
+import { useDispatch } from "react-redux";
+import { addToBasket, removeFromBasket } from "../slices/basketSlice";
+
+const CheckoutProduct = ({
+  id,
+  title,
+  price,
+  rating,
+  description,
+  category,
+  image,
+  hasPrime,
+}) => {
+  const dispatch = useDispatch();
+
+  const addItemToBasket = () => {
+    const product = {
+      id,
+      title,
+      price,
+      rating,
+      description,
+      category,
+      image,
+      hasPrime,
+    };
+    dispatch(addToBasket(product));
+  };
+
+  const removeItemFromBasket = () => {
+    // remove item from redux
+    dispatch(removeFromBasket({ id }));
+  };
+
+  return (
+    <div className="grid grid-cols-5">
+      {/* left */}
+      <Image src={image} height={200} width={200} objectFit="contain" />
+      {/* middle */}
+      <div className="col-span-3 mx-5">
+        <p>{title}</p>
+        <div className="flex">
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <StarIcon key={i} className="h-5 text-yellow-500 " />
+            ))}
+        </div>
+        <p className="text-xs my-2 line-clamp-3">{description}</p>
+        <Currency quantity={price} currency="USD" />
+        {hasPrime && (
+          <div className="flex items-center space-x-2">
+            <img
+              loading="lazy"
+              className="w-12"
+              src="https://links.papareact.com/fdw"
+              alt=""
+            />
+            <p className="text-xs text-gray-500">Free Next-day Delivery</p>
+          </div>
+        )}
+      </div>
+
+      {/* right  add/remove buttons*/}
+      <div className="flex flex-col space-y-2 my-auto justify-self-end">
+        <button className="button mt-auto" onClick={addItemToBasket}>
+          Add to Basket
+        </button>
+        <button onClick={removeItemFromBasket} className="button mt-auto">
+          Remove from Basket
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default CheckoutProduct;
